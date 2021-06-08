6d7198e9713e5d0214db624a1dbb86675c82732c
Hem Shrestha <hereshem@gmail.com>
2021-06-08 16:38:15 +0545

    database.js added

---
+++ config/database.js
@@ -0,0 +1,49 @@
+module.exports = {
+  development: {
+    username: 'user',
+    password: 'P@ssw0rd123',
+    database: 'db',
+    host: 'zerone-stable-addon-806-15-postgresql.zerone-stable-503-806.svc.cluster.local',
+    dialect: 'postgres'
+  },
+  test: {
+    username: 'root',
+    password: null,
+    database: 'boilerplate_test',
+    host: '127.0.0.1',
+    dialect: 'postgres',
+    logging: null
+  },
+  production: process.env.DATABASE_URL
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+^G Get Help                 ^O WriteOut                 ^R Read File                ^Y Prev Page                ^K Cut Text                 ^C Cur Pos
+^X Exit                     ^J Justify                  ^W Where Is                 ^V Next Page                ^U UnCut Text               ^T To Spell
