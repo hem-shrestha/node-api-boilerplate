be744f96d0e0484fa5774a5a4d6caad878765142
Hem Shrestha <hereshem@gmail.com>
2021-06-08 16:41:17 +0545

    update database.js

--- config/database.js
+++ config/database.js
@@ -16,34 +16,3 @@ module.exports = {
   },
   production: process.env.DATABASE_URL
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-^G Get Help                 ^O WriteOut                 ^R Read File                ^Y Prev Page                ^K Cut Text                 ^C Cur Pos
-^X Exit                     ^J Justify                  ^W Where Is                 ^V Next Page                ^U UnCut Text               ^T To Spell
